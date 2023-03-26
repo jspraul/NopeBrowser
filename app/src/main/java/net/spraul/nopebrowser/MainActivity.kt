02bb5e39c543b109c525487a1b71e202b2786fb7
Jed Spraul <jspraul@yahoo.com>
2023-03-26 13:25:34 -0400

    Simon (ChatGPT) says...

--- app/src/main/java/net/spraul/nopebrowser/MainActivity.kt
+++ app/src/main/java/net/spraul/nopebrowser/MainActivity.kt
@@ -1,11 +1,30 @@
 package net.spraul.nopebrowser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set up the WebView
+        webview.webViewClient = WebViewClient()
+        webview.settings.javaScriptEnabled = true
+
+        // Set up the button click listeners
+        button1.setOnClickListener { loadWebsite("https://www.google.com") }
+        button2.setOnClickListener { loadWebsite("https://www.youtube.com") }
+        button3.setOnClickListener { loadWebsite("https://www.reddit.com") }
+    }
+
+    // Load the specified URL in the WebView
+    private fun loadWebsite(url: String) {
+        webview.loadUrl(url)
     }
 }
+
