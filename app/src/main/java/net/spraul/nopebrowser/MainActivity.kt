ad814ecd9341d1e4d77cc27daed002feda2c874b
Jed Spraul <jspraul@yahoo.com>
2023-03-26 22:25:11 -0400

    First pass at URL blocking

--- app/src/main/java/net/spraul/nopebrowser/MainActivity.kt
+++ app/src/main/java/net/spraul/nopebrowser/MainActivity.kt
@@ -8,8 +8,24 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import net.spraul.nopebrowser.databinding.ActivityMainBinding
+import java.io.ByteArrayInputStream
 
 private const val TAG = "NopeBrowser"
+private val denyUrlContains = arrayOf(
+    "youtube.com/watch"
+)
+private val allowUrlContains = arrayOf(
+    "192.168.1.1",
+    "polyfill.io", "www.google-analytics.com", "www.google.com",
+    "abcmouse",
+    "abeka",
+      //"js.monitor.azure.com", "applicationinsights.azure.com",
+      //"static.hotjar.com", "script.hotjar.com",
+      //"apps.usw2.pure.cloud",
+    "babbel", "cloudfront",
+    "goodandbeautiful", "scratch", "vimeocdn", "player.vimeo.com",
+    "youtube", "googlevideo", "gstatic"
+)
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,15 +36,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.webView.settings.allowFileAccess = false
         binding.webView.settings.javaScriptEnabled = true
+        binding.webView.settings.domStorageEnabled = true
+
         binding.webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(
                 view: WebView,
                 request: WebResourceRequest
             ): Boolean {
                 val url = request.url.toString()
-                Log.i(TAG, "Allowing URL: $url")
-                return super.shouldOverrideUrlLoading(view, request)
+                return if (allowUrl(url)) {
+                    Log.i(TAG, "Allowing URL: $url")
+                    super.shouldOverrideUrlLoading(view, request)
+                } else {
+                    Log.i(TAG, "Blocking URL: $url")
+                    true
+                }
             }
 
             override fun shouldInterceptRequest(
@@ -36,8 +60,13 @@ class MainActivity : AppCompatActivity() {
                 request: WebResourceRequest?
             ): WebResourceResponse? {
                 val url = request?.url.toString()
-                Log.i(TAG, "Not replacing URL content: $url")
-                return super.shouldInterceptRequest(view, request)
+                return if (allowUrl(url)) {
+                    Log.i(TAG, "Allowed content: $url")
+                    super.shouldInterceptRequest(view, request)
+                } else {
+                    Log.i(TAG, "Blocking content: $url")
+                    WebResourceResponse("text/plain", "UTF-8", ByteArrayInputStream(ByteArray(0)))
+                }
             }
         }
 
@@ -59,5 +88,18 @@ class MainActivity : AppCompatActivity() {
             binding.webView.goBack()
         else
             super.onBackPressed()
+    }
+
+    fun allowUrl(url: String): Boolean {
+        for (str in denyUrlContains) {
+            if (url.contains(str))
+                return false
+        }
+        for (str in allowUrlContains) {
+            if (url.contains(str)) {
+                return true
+            }
+        }
+        return false
     }
 }
